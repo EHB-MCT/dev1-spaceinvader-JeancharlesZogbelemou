8e9c0473d5c018139d868f11e2039dc35f464824
Jean Charles Junior <jean-charles.zogbelemou@student.ehb.be>
2022-10-11 13:20:33 +0200

    Space invader afgewerkt

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,40 @@
 "use strict";
 
-alert("Draw your space invader here");
+
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext ("2d");
+
+blackSquare();
+greenSquare();
+function blackSquare() {
+    context.beginPath();
+    context.rect(0,0,300,300);
+    context.fillStyle="black";
+    context.fill();
+    context.stroke();
+}
+
+function greenSquare(){
+    context.beginPath();
+    context.rect(120,0,50,50);
+    context.rect(120,50,50,50);
+    context.rect(120,100,50,50);
+    context.rect(20,100,50,50);
+    context.rect(70,100,50,50);
+    context.rect(120,70,50,50);
+    context.rect(170,100,50,50);
+    context.rect(220,100,50,50);
+    context.rect(70,150,50,50);
+    context.rect(70,200,50,50);
+    context.rect(20,200,50,50);
+    context.rect(170,150,50,50);
+    context.rect(170,200,50,50);
+    context.rect(220,200,50,50);
+    context.fillStyle="#66ff00";
+    context.strokeStyle="#66ff00";
+    context.fill();
+    context.stroke();
+
+}
